16da7a4252ee76f04b4421b4fd11014598d3e596
Annie <happy90401@goods10.(none)>
2018-01-17 11:01:49 +0800

    add alias, gref

--- aliases/available/git.aliases.bash
+++ aliases/available/git.aliases.bash
@@ -59,6 +59,7 @@ alias gdel='git branch -D'
 alias gmu='git fetch origin -v; git fetch upstream -v; git merge upstream/master'
 alias gll='git log --graph --pretty=oneline --abbrev-commit'
 alias gg="git log --graph --pretty=format:'%C(bold)%h%Creset%C(magenta)%d%Creset %s %C(yellow)<%an> %C(cyan)(%cr)%Creset' --abbrev-commit --date=relative"
+alias gref="git reflog"
 alias ggs="gg --stat"
 alias gsl="git shortlog -sn"
 alias gwc="git whatchanged"
